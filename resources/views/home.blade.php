a20eff91b6fb3fc0015176e49ce924fd73a9539b
Eugen <epopescuu@yahoo.it>
2024-01-29 10:44:15 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Dati da config/data.js: {{ $home }}</h1>
+    <h3>This is my Homepage content.</h3>
+    <button class="btn btn-primary">Cliccami</button>
+@endsection
